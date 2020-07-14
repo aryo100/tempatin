a695be5d22a85a241fb37c8ab4df7959b1381649
aryo <aryo100@gmail.com>
2020-07-14 22:36:12 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -74,6 +74,11 @@
 											@endif
 											@if (\Session::has('success'))
 												<div class="alert alert-success">
+												<button type="button" class="close" data-dismiss="alert">
+													<i class="ace-icon fa fa-times"></i>
+												</button>
+
+												<i class="ace-icon fa fa-check"></i>
 												<p>{{ \Session::get('success') }}</p>
 												</div><br/>
 											@endif
